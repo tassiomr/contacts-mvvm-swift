23c17d31c04fc3f7e53e6b4b7305cb2898da64a9
Tassio Rocha <tassiomarcosr@outlook.com>
2020-05-14 18:26:32 -0300

    improviment: remove navigation title from scene

--- ContactsMVVM-Swift/SceneDelegate.swift
+++ ContactsMVVM-Swift/SceneDelegate.swift
@@ -21,7 +21,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let navigationController = UINavigationController();
-        navigationController.navigationBar.prefersLargeTitles = true;
         coordinator = MainCoordinator(navigationController: navigationController);
         coordinator?.start();
                 
