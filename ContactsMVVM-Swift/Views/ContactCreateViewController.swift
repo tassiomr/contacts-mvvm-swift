0495bfaa6437b3ea65d278813de28b2c56e0ee15
Tassio Rocha <tassiomarcosr@outlook.com>
2020-05-14 14:10:45 -0300

    feat: add create contact

--- ContactsMVVM-Swift/Views/ContactCreateViewController.swift
+++ ContactsMVVM-Swift/Views/ContactCreateViewController.swift
@@ -8,12 +8,30 @@
 
 import Foundation
 import UIKit
+import Realm
+import RealmSwift
 
 
 class ContactCreateViewController : UIViewController, Storyboarded {
     weak var coordinator: MainCoordinator?
+    var realm: Realm?;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        self.realm = try! Realm()
+    }
+    
+    
+    @IBAction func addNewContact () {
+        let realm = try! Realm()
+        try! realm.write {
+            let contact = Contact();
+            contact.email = "tassiomr@gmail.com"
+            contact.name = "Vanda"
+            contact.phone = "+5531994731494"
+            
+            realm.add(contact)
+        }
     }
 }
