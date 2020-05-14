f0668d2cc7ca8c4de0886897b2be074b7051a894
Tássio Rocha <tassio.rocha@bume.io>
2020-05-14 12:21:38 -0300

    feat: extend Storyboarded protocol

--- ContactsMVVM-Swift/Views/ViewController.swift
+++ ContactsMVVM-Swift/Views/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, Storyboarded {
 
+    weak var coodinator: MainCoordinator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
+    
 }
 
