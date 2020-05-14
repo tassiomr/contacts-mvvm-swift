2987d159f477be01f827a1e16bf00c195e2e0e14
Tassio Rocha <tassiomarcosr@outlook.com>
2020-05-14 14:11:15 -0300

    feat: add tableview source

--- ContactsMVVM-Swift/Views/ViewController.swift
+++ ContactsMVVM-Swift/Views/ViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
+import Realm
 
-class ViewController: UIViewController, Storyboarded {
-
+class ViewController: UIViewController, Storyboarded,UITableViewDelegate, UITableViewDataSource {
+    
     weak var coodinator: MainCoordinator?
+    @IBOutlet weak var tableView: UITableView!
+    var button: UIButton?
+    var contacts = [Contact]()
+    var realm: Realm?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        self.getContacts()
+
+        do {
+            self.realm = try Realm()
+        } catch let error  as NSError {
+            print(error)
+        }
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:            IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell");
+        cell.textLabel?.text = self.contacts[indexPath.row].name;
+        
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contacts.count
+    }
+    
+    func getContacts () {
+        let realm = try! Realm()
+        let contacts = realm.objects(Contact.self)
+        
+        for contact in contacts {
+            print(contact)
+            self.contacts.append(contact)
+        }
+        self.tableView.reloadData();
+    }
 }
 
