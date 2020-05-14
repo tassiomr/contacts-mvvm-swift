83258bebb32b0d5443400fbba12a0a98f438601b
Tássio Rocha <tassio.rocha@bume.io>
2020-05-14 12:20:19 -0300

    feat: create Coordinator protocol

---
+++ ContactsMVVM-Swift/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  ContactsMVVM-Swift
+//
+//  Created by Tássio Marcos Rocha on 14/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    func start()
+}
