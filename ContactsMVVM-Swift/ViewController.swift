e0ed0a843e4be3da4655af842ee13f6be633ecd9
Tássio Rocha <tassio.rocha@bume.io>
2020-05-14 10:04:40 -0300

    Initial Commit

---
+++ ContactsMVVM-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsMVVM-Swift
+//
+//  Created by Tássio Marcos Rocha on 14/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
