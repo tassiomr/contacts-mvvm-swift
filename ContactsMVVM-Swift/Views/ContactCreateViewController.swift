2046723923d2b1af66b2e9232d2c4e9f0c00bbff
Tássio Rocha <tassio.rocha@bume.io>
2020-05-14 12:21:22 -0300

    feat: create contact create view controller

---
+++ ContactsMVVM-Swift/Views/ContactCreateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactChangesViewController.swift
+//  ContactsMVVM-Swift
+//
+//  Created by Tássio Marcos Rocha on 14/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ContactCreateViewController : UIViewController, Storyboarded {
+    weak var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
