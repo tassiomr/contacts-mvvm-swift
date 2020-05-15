f4be2df6664dbf2a5ea1e4a6a614d0c1ada25933
Tassio Rocha <tassiomarcosr@outlook.com>
2020-05-14 22:21:30 -0300

    feat: create view models

---
+++ ContactsMVVM-Swift/ViewModel/MainVIewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainVIewModel.swift
+//  ContactsMVVM-Swift
+//
+//  Created by Tássio Marcos Rocha on 14/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+
+class MainViewModel {
+}
