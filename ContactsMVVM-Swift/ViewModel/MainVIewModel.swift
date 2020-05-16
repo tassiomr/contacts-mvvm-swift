9e076a0b10bade03b32a263158e33a623b1585c3
Tassio Rocha <tassiomarcosr@outlook.com>
2020-05-16 14:21:19 -0300

    feat: create view model

--- ContactsMVVM-Swift/ViewModel/MainVIewModel.swift
+++ ContactsMVVM-Swift/ViewModel/MainVIewModel.swift
@@ -7,9 +7,47 @@
 //
 
 import Foundation
-
 import RxSwift
 
+protocol MainProtocol {
+    func getContact()
+    func deleteContact(completion: (Contact) -> Void)
+    func updateContact(completion: (Contact) -> Void)
+}
+
+enum Actions {
+    case createUpdate
+    case delete
+}
 
 class MainViewModel {
+    
+    var contacts: [Contact] = [Contact]()
+    var service: ContactService
+    
+    init(service: ContactService) {
+        self.service = service;
+        self.getContacts()
+    }
+    
+    func getContacts() {
+        let results = self.service.getAllUser()
+        
+        for contact in results {
+            self.contacts.append(contact)
+        }
+    }
+    
+    func deleteContact(_ contact: Contact, success: () -> Void) {
+        self.service.deleteUser(contact: contact, success: {
+            success()
+        }) {
+            return
+        }
+    }
+        
+    private func reloadList(action: Actions, contact: Contact?) {
+        self.getContacts()
+    }
+    
 }
